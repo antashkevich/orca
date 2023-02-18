7127aa9d7c5b83cf4af0a9f50bccfa6908e8b551
Aleksandr Antashkevich <antoha_ale6@mail.ru>
2023-02-18 12:50:10 +0100

    change dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import Icon from './Icon';
+import styled from 'styled-components';
+import * as styles from '../styles/variables';
+
+const DropdownContainer = styled.div`
+  position: relative;
+`;
+
+const DropdownBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  height: 30px;
+  background-color: ${styles.color_white};
+  border: 1px solid ${styles.color_light_grey};
+  border-radius: 4px;
+  transition: ${styles.transition};
+  padding-left: 8px;
+  padding-right: 8px;
+
+  &:hover {
+    border-color: ${styles.color_blue};
+  }
+
+  ${DropdownContainer}:hover & {
+    border-color: ${styles.color_blue};
+  }
+`;
 
 const Dropdown = (props, {className = ""}) => {
+  debugger
   return (
-    <div className={`dropdown-container ${className}`}>
-      <button className="dropdown-container__btn">
+    <DropdownContainer className={`dropdown-container ${className}`}>
+      <DropdownBtn className="dropdown-container__btn">
         <Icon className="dropdown-container__icon" name={props.dropdown.icon} />
         {props.dropdown.name}
         <Icon className="dropdown-container__icon-arrow" name="arrow" />
-      </button>
+      </DropdownBtn>
       <div className="dropdown-container__list-container">
         <ul className="dropdown-container__list">
           {props.dropdown.items.map((item, id) => 
@@ -18,7 +47,7 @@ const Dropdown = (props, {className = ""}) => {
           )}
         </ul>
       </div>
-    </div>
+    </DropdownContainer>
   );
 }
 
---
+++ src/styles/variables.js
@@ -0,0 +1,20 @@
+// Color
+export const  color_black = '#10111D';
+export const  color_bg = '#f3f4f9';
+export const  color_light_grey = '#CACFDB';
+export const  color_grey = '#70707B';
+export const  color_white = '#FFFFFF';
+export const  color_white_light = '#FDFDFF';
+export const  color_white_grey = '#E1EEFD';
+export const  color_blue = '#0080FF';
+export const  color_high = '#E2005E';
+export const  color_medium = '#FE7B1F';
+export const  color_low = '#F8CD39';
+export const  color_scrollbar = '#F8F8F8';
+export const  color_scrollbar_thumb = '#B5B5BD';
+
+// Font weights
+export const font_weight_normal = '400';
+
+// transition: 
+export const transition = '0.3s';
